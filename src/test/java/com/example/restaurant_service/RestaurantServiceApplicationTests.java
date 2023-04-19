c18370b081743f51438cb48ae5e62f3c837b0ba7
Lahiru Chalana <lahiruchalanairesh@gmail.com>
2023-04-19 14:32:59 +0530

    first commit

---
+++ src/test/java/com/example/restaurant_service/RestaurantServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restaurant_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
