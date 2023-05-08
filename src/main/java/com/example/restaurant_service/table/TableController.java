775883098e4a0fc2af0547998ec48702dd92fe5f
Lahiru Chalana <lahiruchalanairesh@gmail.com>
2023-05-08 10:55:09 +0530

    completed add or update end point in table

--- src/main/java/com/example/restaurant_service/table/TableController.java
+++ src/main/java/com/example/restaurant_service/table/TableController.java
@@ -1,7 +1,13 @@
 package com.example.restaurant_service.table;
 
+import com.example.restaurant_service.dto.RestaurantTypeDTO;
+import com.example.restaurant_service.dto.TableDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +17,14 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class TableController {
 
+    private final TableService tableService;
+
+    @PostMapping("/add-or-update")
+    public ResponseEntity<TableDTO> addOrUpdateTable(
+            @RequestBody TableDTO tableDTO
+    ) {
+        log.info ("LOG :: TableController addOrUpdateTable()");
+        return new ResponseEntity<>(tableService.addOrUpdateTable(tableDTO), HttpStatus.OK);
+    }
 
 }
